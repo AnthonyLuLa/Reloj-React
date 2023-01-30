f88f19a5e8077616b641d92f333371d58a4530b1
unknown <anthonylunaleal@gmail.com>
2023-01-30 17:37:38 -0600

    first commit

---
+++ otraapp/src/App.js
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client';
+import './App.css';
+
+function App() {
+  const root= ReactDOM.createRoot(document.getElementById('root'));
+    const element= (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>La hora es</h1>
+        <h2>Son las {new Date().toLocaleTimeString()} </h2>
+      </header>
+    </div>
+  );
+
+  root.render(element)
+
+}
+
+
+export default App;
